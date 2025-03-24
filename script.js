9b93b25708d27d6af252be00a504adb3673acfe7
Isadora <isadora_sozo-silva@estudante.sesisenai.org.br>
2025-03-24 08:28:15 -0300

    adicionando poliformismo (cores)

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const ctx = canvas.getContext('2d')
 
 
 const teclasPressionadas = {
-   KeyW : false,
+   KeyW: false,
    KeyS: false,
    KeyD: false,
    KeyA: false
 };
+
 document.addEventListener('keydown', (e) => {
    for (let tecla in teclasPressionadas) {
-       if (teclasPressionadas.hasOwnProperty(e.code)) {
+       if (teclasPressionadas.hasOwnProperty(tecla)) {
            teclasPressionadas[tecla] = false;
        }
    }
@@ -19,7 +20,6 @@ document.addEventListener('keydown', (e) => {
    }
 });
 
-
 class Entidade {
    constructor(x, y, largura, altura) {
        this.x = x
@@ -27,60 +27,64 @@ class Entidade {
        this.largura = largura
        this.altura = altura
    }
-   desenhar (){
-       ctx.fillStyle = 'purple'
-       ctx.fillRect(this.x, this.y, this.largura, this.altura)
-   }
+   
 }
-
 
 class Cobra extends Entidade {
    constructor(x, y, largura, altura) {
-       super(x, y, largura, altura)
+       super(x, y, largura, altura);
    }
    atualizar() {
        if (teclasPressionadas.KeyW) {
-           this.y -= 7
+           this.y -= 7;
        } else if (teclasPressionadas.KeyS) {
-           this.y += 7
+           this.y += 7;
        } else if (teclasPressionadas.KeyA) {
-           this.x -= 7
+           this.x -= 7;
        } else if (teclasPressionadas.KeyD) {
-           this.x += 7
+           this.x += 7;
        }
    }
-   verificarColisao(comida){
-       if(
+   desenhar() {
+       ctx.fillStyle = 'purple'; 
+       ctx.fillRect(this.x, this.y, this.largura, this.altura);
+   }
+   verificarColisao(comida) {
+       if (
            this.x < comida.x + comida.largura &&
            this.x + this.largura > comida.x &&
            this.y < comida.y + comida.altura &&
            this.y + this.altura > comida.y
-       ){ 
-           this.#houveColisao(comida)
+       ) {
+           this.#houveColisao(comida);
        }
    }
-   #houveColisao(comida){
-       comida.x = Math.random()*canvas.width-10
-       comida.y = Math.random()*canvas.height-10
+   #houveColisao(comida) {
+       comida.x = Math.random() * (canvas.width - comida.largura);
+       comida.y = Math.random() * (canvas.height - comida.altura);
    }
 }
+
 class Comida extends Entidade {
    constructor() {
-       super(Math.random()*canvas.width-10,Math.random()*canvas.height-10, 20, 20)
+       super(Math.random() * (canvas.width - 20), Math.random() * (canvas.height - 20), 20, 20);
+   }
+   desenhar() {
+       ctx.fillStyle = 'red';
+       ctx.fillRect(this.x, this.y, this.largura, this.altura);
    }
 }
 
-
-const cobra = new Cobra(100, 200, 20, 20)
-const comida = new Comida()
-
+const cobra = new Cobra(100, 200, 20, 20);
+const comida = new Comida();
 
 function loop() {
-   ctx.clearRect(0, 0, canvas.width, canvas.height)
-   cobra.desenhar()
-   cobra.atualizar()
-   comida.desenhar()
-   cobra.verificarColisao(comida)
-   requestAnimationFrame(loop)
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   cobra.desenhar();
+   cobra.atualizar();
+   comida.desenhar();
+   cobra.verificarColisao(comida);
+   requestAnimationFrame(loop);
 }
-loop()
+
+loop();
